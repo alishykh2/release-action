cee2ba4bb1c2edadd83a5b7ea47749837ee4a234
Ali Ahmad <ali.ahmed@tintash.com>
2023-01-05 18:45:10 +0500

    feat: dependencies updated

--- release.config.js
+++ release.config.js
@@ -12,7 +12,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/*.js.map", "package.json"],
+        assets: ["package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
 
-const person = core.getInput("person6");
+const person = core.getInput("person7");
 
 console.log(`Test`);
