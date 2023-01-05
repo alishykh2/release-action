a375852154fa5ef806a5fa4b27f03cdc671b5ea0
Ali Ahmad <ali.ahmed@tintash.com>
2023-01-05 14:04:24 +0500

    feat: updated npm pack2

--- release.config.js
+++ release.config.js
@@ -12,7 +12,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/*.js.map"],
+        assets: ["dist/*.js", "dist/*.js.map", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
