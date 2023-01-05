c6efabb600e679581d3bdc47e83593186546975b
Ali Ahmad <ali.ahmed@tintash.com>
2023-01-05 14:12:34 +0500

    feat: updated npm pack3

--- release.config.js
+++ release.config.js
@@ -17,7 +17,7 @@ const config = {
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic-release/github",
+    // "@semantic-release/github",
   ],
 };
 
